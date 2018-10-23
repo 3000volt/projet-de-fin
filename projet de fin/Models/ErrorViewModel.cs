f359ed4d6e6c975c13d3ae60d9a061b2b623c7ea
3000volt <1591982@csimple.org>
2018-10-23 08:47:38 -0400

    Ajoutez des fichiers projet.

---
+++ projet de fin/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace projet_de_fin.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
